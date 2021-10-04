ae5a536a0533ea6b0cb40e9cfc5dfbdbea4891b8
Peter Mashin <petemashine@gmail.com>
2021-10-04 21:48:31 +0300

    litle update

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
-const container = document.querySelector('.container') ;
+const container = document.querySelector('.container');
 
-const items = document.querySelectorAll('button') ; 
+const items = document.querySelectorAll('button');
 
-const oponent = document.querySelector('.oponent') ;
+const oponent = document.querySelector('.oponent');
 
-const result = document.querySelector('.result') ;
+const result = document.querySelector('.result');
 
-let roundNumber = document.querySelector('.roundnumber') ;
+let roundNumber = document.querySelector('.roundnumber');
 
-let winscore = document.querySelector('.winscore') ;
-let losesscore = document.querySelector('.losesscore') ;
-let drawscore = document.querySelector('.drawscore') ;
+let winscore = document.querySelector('.winscore');
+let losesscore = document.querySelector('.losesscore');
+let drawscore = document.querySelector('.drawscore');
 
-let counter = 0 ;
+let counter = 0;
 
-let drawCounter = 0 ;
-let winCounter = 0 ;
-let losesCounter = 0 ;
+let drawCounter = 0;
+let winCounter = 0;
+let losesCounter = 0;
 
-function oponentChoise (ev) {
-   if (ev.target.closest('.button')) {
-   const chosenIndex = Math.round((Math.random() * (items.length -1))) ;
-   oponent.innerText = items[chosenIndex].innerText ;
-   comprasion(ev) ;
-//    return oponent.innerText ;
-}
+function oponentChoise(ev) {
+    if (ev.target.closest('.button')) {
+        const chosenIndex = Math.round((Math.random() * (items.length - 1)));
+        oponent.innerText = items[chosenIndex].innerText;
+        comprasion(ev);
+        //    return oponent.innerText ;
+    }
 }
 
 function comprasion(ev) {
     // if (ev.target.closest('.button')) {
-    const weap = ev.target.dataset.value ;
-        if (weap === 'stone') {            
-            if (oponent.innerText === 'stone' )  result.innerText = "draw" ;
-            if (oponent.innerText === 'paper' )  result.innerText = "you lose" ;
-            if (oponent.innerText === 'scissors' ) result.innerText = "you win" ;
-            } else if(weap === 'paper' ){
-                if (oponent.innerText === 'stone' ) result.innerText = "you win" ;
-                if (oponent.innerText === 'paper' ) result.innerText = "draw" ;
-                if (oponent.innerText === 'scissors' ) result.innerText = "you lose" ;
-                    }else if(weap ==='scissors') {
-                        if (oponent.innerText === 'stone' ) result.innerText = "you lose" ;
-                        if (oponent.innerText === 'paper' ) result.innerText = "you win" ;
-                        if (oponent.innerText === 'scissors' ) result.innerText = "draw" ;
-            // }
-            
-        }
-        
-        roundCounter() ;
-        scoreCounter() ;
-}
+    const weap = ev.target.dataset.value;
+    if (weap === 'stone') {
+        if (oponent.innerText === 'stone') result.innerText = "draw";
+        if (oponent.innerText === 'paper') result.innerText = "you lose";
+        if (oponent.innerText === 'scissors') result.innerText = "you win";
+    } else if (weap === 'paper') {
+        if (oponent.innerText === 'stone') result.innerText = "you win";
+        if (oponent.innerText === 'paper') result.innerText = "draw";
+        if (oponent.innerText === 'scissors') result.innerText = "you lose";
+    } else if (weap === 'scissors') {
+        if (oponent.innerText === 'stone') result.innerText = "you lose";
+        if (oponent.innerText === 'paper') result.innerText = "you win";
+        if (oponent.innerText === 'scissors') result.innerText = "draw";
+        // }
 
-function roundCounter(){
-        counter+=1 ;
-        roundNumber.innerText=counter ;
-}
-
-function scoreCounter(){
-    if (result.innerText ==="you lose") {
-        losesCounter+=1 ;
-        losesscore.innerText=losesCounter ;
-    }
-    if (result.innerText ==="you win"){
-        winCounter+=1 ;
-        winscore.innerText=winCounter ;
-    } 
-    if (result.innerText ==="draw") {
-        drawCounter+=1
-        drawscore.innerText=drawCounter ;
     }
 
+    roundCounter();
+    scoreCounter();
+}
+
+function roundCounter() {
+    counter += 1;
+    roundNumber.innerText = counter;
+}
+
+function scoreCounter() {
+    if (result.innerText === "you lose") {
+        losesCounter += 1;
+        losesscore.innerText = losesCounter;
+    }
+    if (result.innerText === "you win") {
+        winCounter += 1;
+        winscore.innerText = winCounter;
+    }
+    if (result.innerText === "draw") {
+        drawCounter += 1
+        drawscore.innerText = drawCounter;
+    }
+
 }
 
 
-container.addEventListener('click' , (event) => {oponentChoise(event)}) ;
+container.addEventListener('click', (event) => {
+    oponentChoise(event)
+});
 
 // container.addEventListener('click' , (event) => {comprasion(event)}) ;
 // container.addEventListener('click' , roundCounter ) ;
-
